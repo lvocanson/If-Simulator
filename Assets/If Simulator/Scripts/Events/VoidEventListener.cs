c977a362a567874898ea4fbd97263dc34b9d7abd
Lucas Vocanson <lucas.vocanson@gmail.com>
2023-12-10 13:44:18 +0100

    Create Events

---
+++ Assets/If Simulator/Scripts/Events/VoidEventListener.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+/// <summary>
+/// A listener that listens to a VoidEvent and invokes a UnityEvent when the VoidEvent is raised.
+/// </summary>
+public class VoidEventListener : MonoBehaviour
+{
+    [SerializeField, Tooltip("Event to register with.")]
+    private VoidEvent _event;
+
+    [SerializeField, Tooltip("Response to invoke when Event is raised.")]
+    private UnityEngine.Events.UnityEvent _response;
+
+    private void OnEnable()
+    {
+        _event.Register(this);
+    }
+
+    private void OnDisable()
+    {
+        _event.Unregister(this);
+    }
+
+    /// <summary>
+    /// Invokes the response of the listener. Usually called by the VoidEvent.
+    /// </summary>
+    public void OnEventRaised()
+    {
+        _response.Invoke();
+    }   
+}
