e6726ebe1f0a82c1feefe2f1622fccfeb9d77f14
ArthurChassaing <94675172+ArthurChassaing@users.noreply.github.com>
2023-12-11 15:02:56 +0100

    Try something

---
+++ Assets/If Simulator/Scripts/Behaviors/Sprinter/Sprinter_Attack.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FiniteStateMachine;
+
+public class Sprinter_Attack : BaseState
+{
+    
+    [SerializeField] private float _attackRange = 1f;
+    [SerializeField] private float _attackDamage = 1f;
+    [SerializeField] private float _attackCooldown = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
