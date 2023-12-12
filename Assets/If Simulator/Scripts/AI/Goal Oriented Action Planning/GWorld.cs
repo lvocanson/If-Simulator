c2109b0852f374ec4cfabda7648ded728526df2f
Gotvna <nvogt@gaming.tech>
2023-12-12 10:05:48 +0100

    Test new Methods

---
+++ Assets/If Simulator/Scripts/AI/Goal Oriented Action Planning/GWorld.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GWorld
+{
+    private static readonly GWorld instance = new GWorld();
+    private static WorldStates world;
+
+    static GWorld()
+    {
+        world = new WorldStates();
+    }
+
+    private GWorld()
+    {
+    }
+
+    public static GWorld Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public WorldStates GetWorld()
+    {
+        return world;
+    }
+}
