a8bfa3582db9d226499d16be246d234bd1ce4344
Gotvna <nvogt@gaming.tech>
2023-12-12 19:59:18 +0100

    Add a new methode of GOAP (WIP)

--- Assets/If Simulator/Scripts/AI/Goal Oriented Action Planning/GAction.cs
+++ Assets/If Simulator/Scripts/AI/Goal Oriented Action Planning/GAction.cs
@@ -1,19 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+using SAP2D;
 
 public abstract class GAction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string actionName = "Action";
+    public float cost = 1.0f;
+    public Transform target;
+    public string targetTag;
+    public float duration = 0;
+    public WorldState[] preConditions;
+    public WorldState[] afterEffects;
+    public SAP2DAgent agent;
+
+    public Dictionary<string, int> preconditions;
+    public Dictionary<string, int> effects;
+
+    public WorldStates agentBeliefs;
+
+    public bool running = false;
+
+    public GAction()
     {
-        
+        preconditions = new Dictionary<string, int>();
+        effects = new Dictionary<string, int>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Awake()
     {
-        
+        agent = gameObject.AddComponent<SAP2DAgent>();
+
+        if (preConditions != null)
+        {
+            foreach (WorldState w in preConditions)
+            {
+                preconditions.Add(w.key, w.value);
+            }
+        }
+
+        if (afterEffects != null)
+        {
+            foreach (WorldState w in afterEffects)
+            {
+                effects.Add(w.key, w.value);
+            }
+        }
     }
+
+    public bool IsAchievable()
+    {
+        return true;
+    }
+
+    public bool IsAchievableGiven(Dictionary<string, int> conditions)
+    {
+        foreach (KeyValuePair<string, int> p in preconditions)
+        {
+            if (!conditions.ContainsKey(p.Key))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public abstract bool PrePerform();
+    public abstract bool PostPerform();
 }
--- Assets/If Simulator/Scripts/AI/Goal Oriented Action Planning/GAgent.cs
+++ Assets/If Simulator/Scripts/AI/Goal Oriented Action Planning/GAgent.cs
@@ -1,18 +1,110 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using SAP2D;
 
+public class SubGoal
+{
+    public Dictionary<string, int> sgoals;
+    public bool remove;
+
+    public SubGoal(string s, int i, bool r)
+    {
+        sgoals = new Dictionary<string, int>();
+        sgoals.Add(s, i);
+        remove = r;
+    }
+}
 public class GAgent : MonoBehaviour
 {
+
+    public List<GAction> actions = new List<GAction>();
+    public Dictionary<SubGoal, int> goals = new Dictionary<SubGoal, int>();
+
+    GPlanner planner;
+    Queue<GAction> actionQueue;
+    public GAction currentAction;
+    SubGoal currentGoal;
+
+    public SAP2DPathfindingConfig config;
+    public Vector2[] path;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GAction[] acts = this.GetComponents<GAction>();
+        foreach (GAction a in acts)
+        {
+            actions.Add(a);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    bool invoked = false;
+
+    void CompleteAction()
     {
-        
+        currentAction.running = false;
+        currentAction.PostPerform();
+        invoked = false;
+    }
+
+    void LateUpdate()
+    {
+        if (currentAction != null && currentAction.running)
+        {
+            if (currentAction.agent.Target && currentAction.agent.path.Length < 1f)
+            {
+                if (!invoked)
+                {
+                    Invoke("Complete Action", currentAction.duration);
+                    invoked = true;
+                }
+            }
+            return;
+        }
+        if (planner == null || actionQueue == null)
+        {
+            planner = new GPlanner();
+
+            var sortedGoals = from entry in goals orderby entry.Value descending select entry;
+
+            foreach(KeyValuePair<SubGoal, int> sg in sortedGoals)
+            {
+                actionQueue = planner.plan(actions, sg.Key.sgoals, null);
+                if (actionQueue != null)
+                {
+                    currentGoal = sg.Key;
+                    break;
+                }
+            }
+        }
+
+        if (actionQueue != null && actionQueue.Count == 0)
+        {
+            if (currentGoal.remove)
+            {
+                goals.Remove(currentGoal);
+            }
+            planner = null;
+        }
+
+        if (actionQueue != null && actionQueue.Count > 0)
+        {
+            currentAction = actionQueue.Dequeue();
+            if (currentAction.PrePerform())
+            {
+                if (currentAction.target == null && currentAction.targetTag != "")
+                {
+                    currentAction.target = GameObject.FindWithTag(currentAction.targetTag).transform;
+                }
+
+                if (currentAction.targetTag != null)
+                {
+                    currentAction.running = true;
+                    currentAction.agent.path = currentAction.agent.GetPath(currentAction.target.position);
+                }
+            }
+        }
     }
 }
