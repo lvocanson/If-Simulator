f5ecba47e9a9e359d76f313188e8a6a88b2dff40
Hugo.M <h.maestracci83@gmail.com>
2023-12-13 20:23:32 +0100

    chore: updated scene

--- Assets/If Simulator/Scripts/Managers/LevelManager.cs
+++ Assets/If Simulator/Scripts/Managers/LevelManager.cs
@@ -7,12 +7,13 @@ public class LevelManager : InGameManager
     [SerializeField] private Level _currentLevel;
     [SerializeField] private GameObject _playerPrefab;
     [SerializeField] private Transform _playerSpawnPoint;
+    [SerializeField] private CurrentPlayerSo _currentPlayerSo;
     
     // TODO: Change type to Player when merged with Arthur
     private PlayerMovement _spawnedPlayer;
     
     public PlayerMovement SpawnedPlayer => _spawnedPlayer;
-    
+    public CurrentPlayerSo CurrentPlayerSo => _currentPlayerSo;
 
     protected override void OnContextInitialized(GameModeStartMode mode)
     {
@@ -22,6 +23,7 @@ public class LevelManager : InGameManager
     protected override void OnContextStarted(GameModeStartMode mode)
     {
         _spawnedPlayer = Instantiate(_playerPrefab, _playerSpawnPoint.position, Quaternion.identity).GetComponent<PlayerMovement>();
+        _currentPlayerSo.Load(_spawnedPlayer);
     }
 
     protected override void OnContextQuit(GameModeQuitMode mode)
