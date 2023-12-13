c51ac69d08963f261fab9e00abe71878a308902d
Hugo.M <h.maestracci83@gmail.com>
2023-12-13 18:42:22 +0100

    feature(LevelManager): Manage the player input located in the app

---
+++ Assets/If Simulator/Scripts/Managers/InputManager.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public InputMode CurrentInputMode => _currentInputMode;
+    public PlayerInput PlayerInput => _playerInput;
+    
+    private InputMode _currentInputMode = InputMode.Gameplay;
+    private PlayerInput _playerInput;
+    private bool _isLocked = false;
+    
+    public Action<InputMode> OnInputModeChanged;
+
+    
+    private void Awake()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+
+        SwitchMode(_currentInputMode);
+    }
+    
+    public void SwitchMode(InputMode inputMode)
+    {
+        string actionMapName = GetInputModeName(inputMode);
+
+        _currentInputMode = inputMode;
+        OnInputModeChanged?.Invoke(_currentInputMode);
+        if (!_isLocked) _playerInput.SwitchCurrentActionMap(actionMapName);
+    }
+    
+    public void Lock()
+    {
+        _playerInput.DeactivateInput();
+        _isLocked = true;
+    }
+
+    public void Unlock()
+    {
+        string actionMapName = GetInputModeName(_currentInputMode);
+
+        _playerInput.SwitchCurrentActionMap(actionMapName);
+        _isLocked = false;
+    }
+    
+    private string GetInputModeName(InputMode inputMode)
+    {
+        return inputMode switch
+        {
+            InputMode.Gameplay => "Gameplay",
+            InputMode.UI => "UI",
+            _ => "",
+        };
+    }
+    
+    public enum InputMode
+    {
+        Gameplay,
+        UI
+    }
+}
