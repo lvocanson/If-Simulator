a5675dc670e8b54219779bc23bc77c30688244d5
Hugo.M <h.maestracci83@gmail.com>
2023-12-13 18:37:47 +0100

    feature: Initial scene that loads before the game
    - Holds the App object which has all the manager references
    - Can be called anywhere in the game

---
+++ Assets/If Simulator/Scripts/Managers/App.cs
@@ -0,0 +1,54 @@
+using System;
+using Managers;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Random = UnityEngine.Random;
+
+public class App : MonoBehaviour
+{
+    #region Singleton
+    public static App Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+            Load();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+    #endregion
+    
+    public GameModeManager GameMode { get; private set; }
+    public BackdropManager Backdrop { get; private set; }
+    public PlayerInput Input { get; private set; }
+    
+    public static InputManager InputManager => Instance._inputManager;
+
+    private InputManager _inputManager;
+    
+    private void Load()
+    {
+        Random.InitState((int)DateTime.Now.Ticks);
+        
+        GameMode = GetComponentInChildren<GameModeManager>();
+        Backdrop = GetComponentInChildren<BackdropManager>();
+        Input = GetComponentInChildren<PlayerInput>();
+        _inputManager = GetComponentInChildren<InputManager>();
+        
+        Application.targetFrameRate = -1;
+        QualitySettings.vSyncCount = 0;
+    }
+}
