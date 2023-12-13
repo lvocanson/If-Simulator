f5881e9b052113b77b155b0383385c8077badd93
Hugo.M <h.maestracci83@gmail.com>
2023-12-13 18:41:26 +0100

    feature(BackdropManager): Performs the transitions to scene and game mode

---
+++ Assets/If Simulator/Scripts/Managers/BackdropManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Managers
+{
+    public class BackdropManager : MonoBehaviour
+    {
+        [SerializeField] private Image _backdrop;
+        [SerializeField] private float _time = 1.5f;
+
+        [SerializeField] private AnimationCurve _curve;
+    
+        public IEnumerator Activate()
+        {
+            _backdrop.gameObject.SetActive(true);
+
+            var timer = 0f;
+            while (timer <= 1f)
+            {
+                timer += Time.unscaledDeltaTime / _time;
+            
+                var newColor = _backdrop.color;
+                newColor.a = _curve.Evaluate(timer);
+                _backdrop.color = newColor;
+            
+                yield return null;
+            }
+        }
+
+        public IEnumerator Release()
+        {
+            var timer = 0f;
+            while (timer <= 1f)
+            {
+                timer += Time.unscaledDeltaTime / _time;
+            
+                var newColor = _backdrop.color;
+                newColor.a = _curve.Evaluate(1 - timer);
+                _backdrop.color = newColor;
+            
+                yield return null;
+            }
+
+            _backdrop.gameObject.SetActive(false);
+        }
+
+        public void SetActivePanel()
+        {
+            _backdrop.gameObject.SetActive(true);
+        }
+    }
+}
+
