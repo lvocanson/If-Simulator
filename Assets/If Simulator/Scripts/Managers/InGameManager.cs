a4690180944bafd92fed70a8f52158ed98a8c0d7
Hugo.M <h.maestracci83@gmail.com>
2023-12-13 18:55:49 +0100

    feature: In-Game manager script that provides the LevelContext loading features

---
+++ Assets/If Simulator/Scripts/Managers/InGameManager.cs
@@ -0,0 +1,27 @@
+﻿using GameMode;
+using UnityEngine;
+
+namespace Managers
+{
+    public abstract class InGameManager : MonoBehaviour
+    {
+        protected virtual void Awake()
+        {
+            LevelContext.Instance.OnInitialized += OnContextInitialized;
+            LevelContext.Instance.OnStarted     += OnContextStarted;
+            LevelContext.Instance.OnQuit        += OnContextQuit;
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (!LevelContext.Instance) return;
+            LevelContext.Instance.OnInitialized -= OnContextInitialized;
+            LevelContext.Instance.OnStarted     -= OnContextStarted;
+            LevelContext.Instance.OnQuit        -= OnContextQuit;
+        }
+
+        protected abstract void OnContextInitialized(GameModeStartMode mode);
+        protected abstract void OnContextStarted(GameModeStartMode mode);
+        protected abstract void OnContextQuit(GameModeQuitMode mode);
+    }
+}
