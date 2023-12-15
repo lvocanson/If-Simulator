34e9850fa590db857c809aa0b6219939d48d43be
Thomas <thomasdelorme56@gmail.com>
2023-12-15 12:30:50 +0100

    feat: Working on chockwave

--- Assets/If Simulator/Code/Scripts/Entity/Player/Ability/List/Spells/ShockWaveBehavior.cs
+++ Assets/If Simulator/Code/Scripts/Entity/Player/Ability/List/Spells/ShockWaveBehavior.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Ability
 {
@@ -8,15 +9,36 @@ namespace Ability
         [SerializeField] private GameObject _swPrefab;
         [SerializeField] private Transform _spawnPoint;
         
+        [Header("Ability Settings")]
         private float _timer;
         [SerializeField] private AnimationCurve _evolutionCurve;
         [SerializeField] private float _maxSize;
         
+        [Header("Ability Damage Management")]
+        [SerializeField] private LayerMask _layers;
+        [SerializeField] private LayerMask _damageableEntityLayers;
+        
+        protected int _ownerLayer;
+        public event Action OnHit;
+        
         private GameObject _swInstance;
 
-        private void OnTriggerEnter(Collider other)
+        private void OnTriggerEnter2D(Collider2D other)
         {
+            int otherLayer = other.gameObject.layer;
             
+            // Skip unwanted layers
+            if (((1 << otherLayer) & _layers.value) == 0) return;
+            if (otherLayer == _ownerLayer) return;
+            
+            // Assert that the other layer is a damageable entity
+            if (((1 << otherLayer) & _damageableEntityLayers.value) == 0) return;
+
+            // Assert that the other entity is a bullet and get its behavior
+            if (!other.TryGetComponent(out BulletBehavior bullet)) return;
+            
+            //bullet.ForceDestroy();
+            OnHit?.Invoke();
         }
         
         protected override void OnEffectStart()
