3d2cd594392ea2ce707c78d139c43e53190b2d13
Gotvna <nvogt@gaming.tech>
2023-12-15 15:23:19 +0100

    Update attackConfig and wonderConfig

--- Assets/If Simulator/Code/Scripts/AI/Goal Oriented Action Planning/Config/WanderConfigSO.cs
+++ Assets/If Simulator/Code/Scripts/AI/Goal Oriented Action Planning/Config/WanderConfigSO.cs
@@ -7,7 +7,7 @@ namespace IfSimulator.GOAP.Config
     [CreateAssetMenu(menuName = "AI/WanderConfig", fileName = "WanderConfig", order = 2)]
     public class WanderConfigSO : ScriptableObject
     {
-        public Vector2 WaitRangeBetweenWanders = new Vector2(1, 5);
+        public Vector2 WaitRangeBetweenWanders = new Vector2(1, 4);
         public float WanderRadius = 5f;
     }
 }
