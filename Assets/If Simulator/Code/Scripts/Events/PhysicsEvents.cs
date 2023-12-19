1ba6b65fe4e4539710755c88c95ceb0cc9796007
Hugo.M <h.maestracci83@gmail.com>
2023-12-19 01:43:57 +0100

    chore: onStay trigger event

--- Assets/If Simulator/Code/Scripts/Events/PhysicsEvents.cs
+++ Assets/If Simulator/Code/Scripts/Events/PhysicsEvents.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class PhysicsEvents : MonoBehaviour
 {
     public event Action<Collider2D> OnEnter;  
+    public event Action<Collider2D> OnStay;
     public event Action<Collider2D> OnExit;
+    
     
     private void OnTriggerEnter2D(Collider2D other)
     {
         OnEnter?.Invoke(other);
+    }
+    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        OnStay?.Invoke(other);
     }
 
     private void OnTriggerExit2D(Collider2D other)
