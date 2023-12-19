1f41d170a25654df300f8366335a7cf30143fa66
Thomas <thomasdelorme56@gmail.com>
2023-12-19 14:36:04 +0100

    feat: Add invisibility feature #WIP : Need to change all enemies detection

---
+++ Assets/If Simulator/Code/Scripts/Entity/Player/Ability/List/Spells/AbilityInvisibility.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Ability
+{
+    public class AbilityInvisibility : AbilityActive
+    {
+        [SerializeField] private CircleCollider2D _playerTrigger;
+        [SerializeField] private SpriteRenderer _playerSprite;
+        
+        [SerializeField] private Color _invisibilityColor;
+        private Color _defaultColor;
+        
+        protected override void OnEffectStart()
+        {
+            _playerTrigger.enabled = false;
+            _defaultColor = _playerSprite.color;
+            _playerSprite.color = _invisibilityColor;
+        }
+
+        protected override void OnEffectUpdate()
+        {
+            //TODO : exit invisibility on attack or hit
+        }
+
+        protected override void OnEffectEnd()
+        {
+            _playerTrigger.enabled = true;
+            _playerSprite.color = _defaultColor;
+        }
+    }
+}
