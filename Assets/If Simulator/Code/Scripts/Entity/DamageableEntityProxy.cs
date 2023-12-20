661f12436fdf917371b43b653cbdf5fc8af01738
Gotvna <nvogt@gaming.tech>
2023-12-20 17:42:38 +0100

    Add Layer + Heal function on EntityProxy

--- Assets/If Simulator/Code/Scripts/Entity/DamageableEntityProxy.cs
+++ Assets/If Simulator/Code/Scripts/Entity/DamageableEntityProxy.cs
@@ -5,4 +5,6 @@ public class DamageableEntityProxy : MonoBehaviour, IDamageable
     [SerializeField] private DamageableEntity _forwardTo;
 
     public void Damage(float damage) => _forwardTo.Damage(damage);
+
+    public void Heal(float heal) => _forwardTo.Heal(heal);
 }
