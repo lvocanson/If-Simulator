ce80c51168da5fadccadbb7842e17735fbe68b08
Hugo.M <h.maestracci83@gmail.com>
2023-12-20 02:34:38 +0100

    feature: basic ability UI + player life UI

---
+++ Assets/If Simulator/Code/Scripts/UI/GameUI/Abilities/LevelableAbilityUI.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelableAbilityUI : AbilityIconUI
+{
+    [SerializeField] private LayoutGroup _levelStarLayoutGroup;
+    [SerializeField] private GameObject _levelStarPrefab;
+    
+    
+    private List<LevelStarUI> _levelStars = new();
+
+
+    private void Awake()
+    {
+        _levelStars.Clear();
+        
+        for (int i = 0; i < _levelStarLayoutGroup.transform.childCount; i++)
+        {
+            _levelStars.Add(_levelStarLayoutGroup.transform.GetChild(i).GetComponent<LevelStarUI>());
+        }
+    }
+
+    public void InitPassiveLevels(int maxLevel)
+    {
+        // TODO : Replace by sprite in so
+        //_icon.sprite = passive.Icon;
+        
+        int levelStarInstantiated = _levelStarLayoutGroup.transform.childCount;
+        
+        if (levelStarInstantiated > maxLevel)
+        {
+            for (int i = maxLevel; i < levelStarInstantiated; i++)
+            {
+                Destroy(_levelStarLayoutGroup.transform.GetChild(i).gameObject);
+                _levelStars.RemoveAt(i);
+            }
+        }
+        else if (levelStarInstantiated < maxLevel)
+        {
+            for (int i = levelStarInstantiated; i < maxLevel; i++)
+            {
+                LevelStarUI star = Instantiate(_levelStarPrefab, _levelStarLayoutGroup.transform).GetComponent<LevelStarUI>();
+                _levelStars.Add(star);
+            }
+        }
+        
+        LevelUpPassive(1);
+    }
+    
+    public void LevelUpPassive(int nextLevel)
+    {
+        for (int i = 0; i < _levelStars.Count; i++)
+        {
+            _levelStars[i].EnableStar(i < nextLevel);
+        }
+    } 
+}
---
+++ Assets/If Simulator/Code/Scripts/UI/GameUI/PlayerUIManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class PlayerUIManager : MonoBehaviour
+{
+    [SerializeField] private PlayerLifeUI _playerLife;
+    [SerializeField] private SpellHolderUI spellHolderUI;
+    [SerializeField] private PassiveHolderUI passiveHolderUI;
+    [SerializeField] private CurrentPlayerSo _currentPlayerSo;
+    
+    
+    public SpellHolderUI SpellHolderUI => spellHolderUI;
+    public PassiveHolderUI PassiveHolderUI => passiveHolderUI;
+
+
+    private void OnEnable()
+    {
+        _currentPlayerSo.OnPlayerLoaded += LoadEvents;
+    }
+    
+    private void LoadEvents()
+    {
+        if (_currentPlayerSo.Player == null)
+        {
+            Debug.LogError("Player is null");
+            return;
+        }
+        _currentPlayerSo.Player.OnHealthChanged += _playerLife.UpdateHealth;
+    }
+    
+    private void OnDisable()
+    {
+        _currentPlayerSo.OnPlayerLoaded -= LoadEvents;
+        UnloadEvents();
+    }
+    
+    private void UnloadEvents()
+    {
+        if (_currentPlayerSo.Player == null)
+        {
+            Debug.LogError("Player is null");
+            return;
+        }
+        
+        _currentPlayerSo.Player.OnHealthChanged -= _playerLife.UpdateHealth;
+    }
+
+    private void Start()
+    {
+        SpellHolderUI.UpdateFirstSpell(null);
+        SpellHolderUI.UpdateSecondSpell(null);
+    }
+}
