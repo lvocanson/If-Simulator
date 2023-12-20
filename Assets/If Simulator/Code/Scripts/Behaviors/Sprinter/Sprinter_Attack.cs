2c84ad83630c21b598f8d188c88376a2cb5beca6
ArthurChassaing <94675172+ArthurChassaing@users.noreply.github.com>
2023-12-20 17:59:17 +0100

    work on sprinter attack

--- Assets/If Simulator/Code/Scripts/Behaviors/Sprinter/Sprinter_Attack.cs
+++ Assets/If Simulator/Code/Scripts/Behaviors/Sprinter/Sprinter_Attack.cs
@@ -8,6 +8,7 @@ public class Sprinter_Attack : BaseState
     [ShowNonSerializedField, Tooltip("The target to move towards")] private Transform _target;
     [Header("References")]
     [SerializeField] private Enemy _enemy;
+    [SerializeField] private CircleCollider2D _hitAttack; 
     
     [Header("State Machine")]
     [SerializeField] private Sprinter_Chase _chaseState;
@@ -34,19 +35,26 @@ public class Sprinter_Attack : BaseState
 
     private IEnumerator Attack()
     {
-        yield return new WaitForSeconds(1f);
-        
-        //TODO ATTACK PLAYER
+        while (_target != null)
+        {
+            Debug.Log("Attack melee");
+            _hitAttack.enabled = true;
+            yield return new WaitForSeconds(0.1f);
+            _hitAttack.enabled = false;
+            yield return new WaitForSeconds(1f);
+        }
     }
 
 
     private void OnDisable()
     {
         _attackEvent.OnExit -= ExitAttackRange;
-        _enemy.Agent.isStopped = false;
         
         if (_enemy.gameObject != null)
             return; // If the enemy is dead, don't do anything
+        
+        _enemy.Agent.isStopped = false;
+        _hitAttack.enabled = false;
         
         if (_attackSprinter != null)
         {
